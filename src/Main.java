d23cf9c4b2c92d8983a1dcac86eba317384ea04b
naveenk197 <31845524+naveenk197@users.noreply.github.com>
2017-09-12 13:16:54 +0530

    ged

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//You may test that your code works find here
+		println//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
 	}
